50f9b75b6295155e803fe2c2b630aa9f9420d6af
Jon McManus <jon@acss.net.au>
2011-03-20 15:22:38 +1000

    Memoize and properly refer to the configuration for databases.

--- util/db.py
+++ util/db.py
@@ -4,8 +4,12 @@ import os
 import sqlite3
 
 import util.config
+import util.memoize
 
 words_schema = ['word', 'length', 'playability', 'playability_order', 'min_playability_order','max_playability_order', 'combinations0', 'probability_order0', 'min_probability_order0', 'max_probability_order0', 'combinations1', 'probability_order1', 'min_probability_order1', 'max_probability_order1', 'combinations2', 'probability_order2', 'min_probability_order2', 'max_probability_order2', 'alphagram', 'num_anagrams', 'num_unique_letters', 'num_vowels', 'point_value', 'front_hooks', 'back_hooks', 'is_front_hook', 'is_back_hook', 'lexicon_symbols', 'definition']
+words_schema2 = ['word', 'length', 'alphagram', 'num_anagrams', 'num_unique_letters', 'num_vowels', 'point_value', 'front_hooks', 'back_hooks', 'is_front_hook', 'is_back_hook', 'lexicon_symbols', 'definition']
+
+words_schema_2 = words_schema[:2] + words_schema[18:]
 
 def dict_factory (cursor, row):
     """
@@ -14,8 +18,10 @@ def dict_factory (cursor, row):
     have the exact specification in list format. Thus, zip that and the row
     together and return it without any interaction with the cursor.
     """
-
-    return dict(zip(words_schema, row))
+    if len(row) != len(words_schema):
+        return dict(zip(words_schema2, row))
+    else:
+        return dict(zip(words_schema, row))
 
 class DatabaseNotFoundError (Exception):
     """
@@ -80,13 +86,15 @@ class Database (object):
             searchlist = search.SearchList(searchlist)
             query, args, functions = searchlist.query()
 
+        print query, args, functions
+
         if functions:
             for fname, fn in functions.items():
-                self.register(fname, fn)
+                self.register(fname, util.memoize.Memoizer(fn))
 
         return self.query(query, args)
 
     connection = property(connect)
 
 def csw ():
-    return Database(config.LEXICONS["CSW"])
+    return Database(util.config.LEXICONS["CSW"])
