bb254d1e97be2b5e0557b0bea691d5c4c4aadc41
Jon McManus <jon@acss.net.au>
2011-03-20 18:56:45 +1000

    Test -- there's no point optimising if it doesn't work.

---
+++ test/test_z___.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import unittest
+
+import util.search, util.db
+
+class TestZ___Anagram (unittest.TestCase):
+
+    def setUp (self):
+        with open("test/z___.txt") as f:
+            self.acceptable = set([line.strip() for line in f])
+
+        self.search = util.search.AnagramMatch("Z[AEIOU][AEIOU]?")
+        self.db = util.db.csw()
+
+    def test_search (self):
+        results = self.db.search(self.search)
+
+        for result in results:
+            word = result["word"]
+            self.assertIn(word, self.acceptable)
