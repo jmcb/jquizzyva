4acadda58fc4be6553857fe8db8f379db98e894f
Jon McManus <jon@acss.net.au>
2011-03-20 19:08:14 +1000

    And test the subanagrams of Z[AEIOU][AEIOU]?.

--- test/test_z___.py
+++ test/test_z___.py
@@ -19,3 +19,19 @@ class TestZ___Anagram (unittest.TestCase):
         for result in results:
             word = result["word"]
             self.assertIn(word, self.acceptable)
+
+class TestZ___SubAnagram (unittest.TestCase):
+
+    def setUp (self):
+        with open("test/sub_z___.txt") as f:
+            self.acceptable = set([line.strip() for line in f])
+
+        self.search = util.search.SubanagramMatch("Z[AEIOU][AEIOU]?")
+        self.db = util.db.csw()
+
+    def test_search (self):
+        results = self.db.search(self.search)
+
+        for result in results:
+            word = result["word"]
+            self.assertIn(word, self.acceptable)
