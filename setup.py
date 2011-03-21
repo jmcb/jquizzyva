46a37250c7dfd12f1c800548b61fadba53d57296
Jon McManus <jon@acss.net.au>
2011-03-21 22:54:38 +1000

    Move the setup file down a step.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-ext_modules = [Extension("_pattern", ["_pattern.c"]),
-               Extension("_search", ["_search.c"]),]
+ext_modules = [Extension("_pattern", ["util/_pattern.c"]),
+               Extension("_search", ["util/_search.c"]),]
 
 setup(
   name = 'jquizzyva extensions',
