90d94a96f1d84806c8a33d5799094f9703d36253
Jon McManus <jon@acss.net.au>
2011-03-24 14:23:04 +1000

    A start at a CGI-based JSON server.

---
+++ server/search.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import cgi
+import json
+import sys
+
+import util.search, util.db
+
+def main (args, cgi_args):
+    search_term = cgi_args.getfirst("term", None)
+
+    if search_term is None:
+        search_term = cgi_args.getfirst("s", None)
+
+    if search_term is None:
+        print json.dumps("No search term provided.")
+        return
+
+    lexicon = cgi_args.getfirst("lexicon", None)
+
+    if lexicon is None:
+        lexicon = cgi_args.getfirst("l", None)
+
+    if lexicon is None:
+        print json.dumps("No lexicon provided.")
+        return
+
+    lex = util.db.lexicon(lexicon)
+
+    if lex is None:
+        print json.dumps("Invalid lexicon '%s'" % lexicon)
+
+    search_term = util.search.SearchList.from_json(search_term)
+
+    result = lex.search(search_term, show_query=False)
+
+    print json.dumps(result)    
+
+if __name__=="__main__":
+    sys.stderr=sys.stdout
+
+    print "Content-type: application/json"
+    print
+
+    try:
+        main (sys.argv, cgi.FieldStorage())
+    except Exception, e:
+        print json.dumps({"exception_type": str(type(e)), "exception_message": str(e))
+        
