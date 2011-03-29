656667019d9174a3390cd10f74c1a791a1d4be54
Jon McManus <jon@acss.net.au>
2011-03-29 12:36:31 +1000

    Allow loading searches from Zyzzyva XML.

--- util/load.py
+++ util/load.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
 
-import lxml
+import os
+
+import lxml.etree
+import lxml.cssselect
 
 import util.search
+
+class UnsupportedSearchType (Exception):
+    pass
+
+class UnsupportedSearchArgument (Exception):
+    pass
 
 SEARCH_TYPES = {
     "Pattern Match": util.search.PatternMatch,
     "Anagram Match": util.search.AnagramMatch,
     "Subanagram Match": util.search.SubanagramMatch,
     "Length": util.search.Length,
+    "Exact Length": util.search.Length,
     "Takes Prefix": util.search.TakesPrefix,
     "Takes Suffix": util.search.TakesSuffix,
     "Includes Letters": util.search.IncludesLetters,
+    "Must Include": util.search.IncludesLetters,
     "Consists of": util.search.ConsistsOf,
     "Belongs to Group": util.search.BelongsToGroup,
     "In Word List": util.search.InWordList,
@@ -36,11 +47,49 @@ class SearchValue (object):
         except:
             return {self.attribute: mvalue}
 
-SEARCH_VALUE_LOOKS = {
-    "string": SearchValue("search_term_string", str),
+SEARCH_VALUES = {
+    "string": SearchValue("search_string", str),
     "min": SearchValue("search_range_start", int),
     "max": SearchValue("search_range_stop", int),
     "bool": SearchValue("negated", bool, False),
     "int": SearchValue("search_range_start", int),
     "number": SearchValue("search_range_start", int),
 }
+
+def parse_condition (condition):
+    args = {}
+
+    condition = dict(condition.items())
+
+    type = condition.pop("type")
+
+    if not SEARCH_TYPES.has_key(type):
+        raise UnsupportedSearchType(type)
+
+    type = SEARCH_TYPES[type]
+
+    for key, value in condition.iteritems():
+        if not SEARCH_VALUES.has_key(key):
+            raise UnsupportedSearchArgument(key)
+
+        args.update(SEARCH_VALUES[key].value(value))
+
+    return type(**args)
+
+def parse_search (document_text):
+    try:
+        document_text = document_text.read()
+    except:
+        if os.path.exists(document_text):
+            document_text = open(document_text).read()
+
+    document = lxml.etree.fromstring(document_text)
+
+    conditions = lxml.cssselect.CSSSelector("and > condition")(document)
+
+    search_list = util.search.SearchList()
+
+    for condition in conditions:
+        search_list.append(parse_condition(condition))
+
+    return search_list
